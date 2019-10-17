8c65dadfe5131df40565d54a2da495c509d26148
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-10-18 02:57:09 +0330

    Added .eslint and modified a bit

--- HW1/index.js
+++ HW1/index.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const whiskers = require('whiskers')
+const express = require('express');
+const whiskers = require('whiskers');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-console.log('--------------------------- NEW APP ')
+/* eslint no-console: ["error", { allow: ["log"] }] */
+console.log('--------------------------- NEW APP ');
 app.use('/', (req, res, next) => {
-    console.log('Middleware Called!')
-    next();
+  console.log('Middleware Called!');
+
+  next();
 });
 
-app.get('/', (req, res) => {
-    console.log(req.query)
+app.get('/gis/testpoint', (req, res) => {
+  console.log(req.query.lat);
+  console.log(req.query.long);
 
-    res.send(whiskers.render(`
+
+  res.send(whiskers.render(`
     <html>
     <body>
     <h1>Hello team a new rendering engine is out!</h1>
@@ -23,7 +27,25 @@ app.get('/', (req, res) => {
     <ul>
     </body>
     </html>
-    `, req))
+    `, req));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/gis/addpolygon', (req, res) => {
+  console.log(req.query.lat);
+  console.log(req.query.long);
+
+
+  res.send(whiskers.render(`
+    <html>
+    <body>
+    <h1>Hello team a new rendering engine is out!</h1>
+    <ul>
+    <li>Foo is:{query.foo}</li>
+    <li>Bar is:{query.bar}</li>
+    <ul>
+    </body>
+    </html>
+    `, req));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
